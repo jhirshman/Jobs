6202767d4093ae113657914d556e4c7711667298
jhirshman <jhirshman@sse.stanford.edu>
2013-04-27 17:18:20 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartXJobs::Application.config.secret_token = '1c3f0dd7e643073eef280f06055b3fb7c9632949c440be603ac991326e2f72e79dee689b81eebc70136411690fda86f22ebd4531647d7b88745db4cf2021981e'
